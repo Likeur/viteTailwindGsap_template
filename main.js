919517f995bb23e5c5dd7473c11e35e8d5b1c749
Likeur <jacksimba28@gmail.com>
2024-03-14 23:39:22 +0200

    modif hero

--- main.js
+++ main.js
@@ -20,4 +20,50 @@ const lenis_fonction = () => {
 lenis_fonction()
 
 
-// your code here 
+// toggle menu
+
+let isMenuOpen = false
+const toggleMenuBtn = document.querySelector('#toggle__menu')
+const tl = gsap.timeline({
+    defaults:{
+        duration: .6,
+        ease:'expo.inOut'
+    }
+})
+gsap.set('.link', {
+    yPercent:100
+})
+
+tl.to('#bar2',{width:'0'})
+  .to('#bar1', {rotate:'45deg', position:'absolute', marginTop:'3px'})
+  .to('#bar3', {rotate:'-45deg', marginTop:'-3px'}, '<')
+  // .to('#toggle__menu', {border:'.3px solid whitesmoke',}, '<')
+  .to('#navigation', {scaleY:1, transformOrigin:'top', duration:1.5}, '<')
+  .to('.link', {yPercent:0, duration:1.5, stagger:.1}, '-=.7')
+
+
+tl.pause()
+toggleMenuBtn.addEventListener('click', ()=>{
+    isMenuOpen = !isMenuOpen
+    if(isMenuOpen){
+        tl.play()
+    } else if(!isMenuOpen){
+        tl.reverse()
+        
+    }
+})
+
+// nav link interraction
+
+const liens = document.querySelectorAll('.link');
+liens.forEach((lien) => {
+  lien.addEventListener('mouseenter', (e) => {
+    const lienSurvole = e.target;
+    const autresLiens = Array.from(liens).filter((l) => l !== lienSurvole);
+    gsap.to(autresLiens, { color: 'rgba(209 213 219 , 0.4)' });
+  });
+    
+  lien.addEventListener('mouseleave', () => {
+    gsap.to(liens, { color: 'white' });
+  });
+});
